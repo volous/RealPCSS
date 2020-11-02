e2921dad14a955bd0d250391d905d45e9b846e8b
Thomas Lindhardt <tlindh19@student.aau.dk>
2020-11-02 11:07:51 +0100

    pressing play starts the game

--- src/Menu.py
+++ src/Menu.py
@@ -11,31 +11,23 @@ class Menu:
         self.menuX = menuX
         self.menuY = menuY
         self.screen = screen
-
-        # instantiating Bomb class
+        self.menu_draw = pgm.Menu(self.menuX, self.menuY, 'Welcome', theme=pgm.themes.THEME_SOLARIZED)
         self.bomb_player_one = Bomb(10, 10, True, 5, True, screen)
-        # instantiating Char class
         self.char1 = Character(249, 149, screen)
         self.level = Level(0, 0, 0, 0, screen)
-
-        self.menu_draw = pgm.Menu(self.menuX, self.menuY, 'Welcome', theme=pgm.themes.THEME_SOLARIZED)
-
-
-
 
     def menu(self):
         self.menu_draw.add_text_input('Name: ')
         self.menu_draw.add_button('Play', self.playButton)
         self.menu_draw.mainloop(self.screen)
 
-
     def playButton(self):
         self.menu_draw.disable()
+        self.screen.fill((0, 0, 0))
         self.level.level()
         self.level.positional_grid()
         self.level.impassible_blocks()
         self.char1.draw_char()
-
 
     def itemShop(self):
         pass
--- src/main.py
+++ src/main.py
@@ -16,22 +16,13 @@ bRadX, bRadY = 10, 10
 screen = pg.display.set_mode((width, height))
 # Initialize the pygame menu, comment out to work on game for now
 menu = Menu(height, width, screen)
-# instantiating Bomb class
-# bomb_player_one = Bomb(bRadX, bRadY, True, 5, True, screen)
-# instantiating Char class
-# char1 = Character(249, 149, screen)
-# level = Level(0, 0, 0, 0, screen)
+
 
 running = True
 # game loop-ish
 while running:
     menu.menu()
-    screen.fill((0, 0, 0))
     pg.time.delay(100)
-
-    # timer is available from start, but when an event type of keydown on space, timer_start from bomb class is set
-    # to true and begins countdown
-    # bomb_player_one.timer()
 
     # checks if there are events in the pygame window
     for event in pg.event.get():
@@ -39,33 +30,7 @@ while running:
         # if the window closes, it gets closed properly
         if event.type == pg.QUIT:
             running = False
-        # if a key is pressed down event is triggered
-        # if event.type == pg.KEYDOWN:
-        #     # if key pressed is space the timer_start is set to true
-        #     if event.key == pg.K_SPACE:
-        #         bomb_player_one.timer_start = True
-        #         bomb_player_one.bomb()
 
         trigger = pg.key.get_pressed()
 
-    # if trigger[pg.K_w] and char1.posY > char1.vel:
-    #     char1.posY -= char1.vel
-    # if trigger[pg.K_s] and char1.posY + char1.vel + char1.height < height:
-    #     char1.posY += char1.vel
-    #
-    # if trigger[pg.K_a] and char1.posX > char1.vel:
-    #     char1.posX -= char1.vel
-    #
-    # if trigger[pg.K_d] and char1.posX + char1.vel + char1.width < width:
-    #     char1.posX += char1.vel
-
-        # if the window closes, it gets closed properly
-        # if event.type == pg.QUIT:
-        #     running = False
-        # # if a key is pressed down event is triggered
-        # if event.type == pg.KEYDOWN:
-        #     # if key pressed is space the timer_start is set to true
-        #     if event.key == pg.K_SPACE:
-        #         bomb_player_one.timer_start = True
-        #         bomb_player_one.bomb(screen)
     pg.display.update()
