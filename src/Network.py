80cd675f64ff993c258f3930f8d55623ef6015b9
Irusu99 <calvin@mikkelsen.nl>
2020-11-14 13:29:37 +0100

    Server, still work in progress

---
+++ src/Network.py
@@ -0,0 +1,19 @@
+import socket
+
+class Network:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = (socket.gethostbyname(socket.gethostname()))
+        self.port = 5555
+        self.address = (self.server, self.port)
+        self.id = self.connect()
+        print(self.id)
+
+    def connect(self):
+        try:
+            self.client.connect(self.address)
+            return self.client.recv(2048).decode()
+        except:
+            pass
+
+n = Network()
