989113f0a043e09e3f193e87dfc6ee71a5048083
Thomas Lindhardt <tlindh19@student.aau.dk>
2020-11-17 09:58:57 +0100

    added if __name__ == '__main__'

--- src/main.py
+++ src/main.py
@@ -1,47 +1,49 @@
 import pygame as pg
 from Menu import Menu
 
-# initialize pygame
-pg.init()
+if __name__ == '__main__':
 
-# Set caption and icon
-pg.display.set_caption("Bomberman Spin-off Game")
-icon = pg.image.load('res/bomb3.png')
-pg.display.set_icon(icon)
+    # initialize pygame
+    pg.init()
 
-# Create surface from dimensions
-width, height = 900, 700
-surface = pg.display.set_mode((width, height))
-game_surface = pg.display.set_mode((width, height))
-# Instantiate the pygame menu
-menu = Menu(height, width, surface, game_surface)
+    # Set caption and icon
+    pg.display.set_caption("Bomberman Spin-off Game")
+    icon = pg.image.load('res/bomb3.png')
+    pg.display.set_icon(icon)
 
-# pygame_menu handles draws itself, so no need to put it in the loop
-menu.draw_mainMenu()
-# menu.draw_itemShopMenu()
-# menu.draw_settingsMenu()
-# menu.draw_quitMenu()
+    # Create surface from dimensions
+    width, height = 900, 700
+    surface = pg.display.set_mode((width, height))
+    game_surface = pg.display.set_mode((width, height))
+    # Instantiate the pygame menu
+    menu = Menu(height, width, surface, game_surface)
 
-# boolean variable that keeps the while loop running
-running = True
-while running:
-    # runs when the play button in the menu is pressed
-    menu.start_game()
-    # delay so the game is slower
-    pg.time.wait(100)
+    # pygame_menu handles draws itself, so no need to put it in the loop
+    menu.draw_mainMenu()
+    # menu.draw_itemShopMenu()
+    # menu.draw_settingsMenu()
+    # menu.draw_quitMenu()
 
-    # checks if there are events in the pygame window
-    events = pg.event.get()
-    for event in events:
-        # if the window closes, it gets closed properly
-        if event.type == pg.QUIT:
-            running = False
+    # boolean variable that keeps the while loop running
+    running = True
+    while running:
+        # runs when the play button in the menu is pressed
+        menu.start_game()
+        # delay so the game is slower
+        pg.time.wait(100)
 
-    if menu.mainMenu.is_enabled():
-        menu.mainMenu.update(events)
-        menu.mainMenu.draw_bomb(surface)
-        menu.mainMenu.mainloop(surface, bgfun=menu.draw_background)
+        # checks if there are events in the pygame window
+        events = pg.event.get()
+        for event in events:
+            # if the window closes, it gets closed properly
+            if event.type == pg.QUIT:
+                running = False
 
-        menu.mainMenu.mainloop(surface)
-    # updates the display of the pygame window
-    pg.display.update()
+        if menu.mainMenu.is_enabled():
+            menu.mainMenu.update(events)
+            menu.mainMenu.draw_bomb(surface)
+            menu.mainMenu.mainloop(surface, bgfun=menu.draw_background)
+
+            menu.mainMenu.mainloop(surface)
+        # updates the display of the pygame window
+        pg.display.update()
